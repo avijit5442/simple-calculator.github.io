74db80a1da4ccb90b0d665fcecbc637f766b736c
Avijit Behera <94885417+avijit5442@users.noreply.github.com>
2022-01-13 20:43:27 +0530

    Add files via upload

---
+++ calc01.js
@@ -0,0 +1,16 @@
+    function sqr(){document.getElementById("result3").innerHTML=Number(Math.pow(number5.value,2))}
+    function cube(){document.getElementById("result3").innerHTML=Number(Math.pow(number5.value,3))}
+    function sqrt(){document.getElementById("result3").innerHTML=Number(Math.sqrt(number5.value))}
+    function sqc(){document.getElementById("result3").innerHTML=Number(Math.pow(number5.value,1/3))}
+    function log(){document.getElementById("result3").innerHTML=Number(Math.log(number5.value))}
+
+        function divide(){document.getElementById("result").innerText=Number(number1.value)/Number(number2.value)}
+        function multi() {document.getElementById("result").innerText=Number(number1.value)*Number(number2.value)}
+        function add() {document.getElementById('result').innerText=Number(number1.value)+Number(number2.value)}
+        function sub(){document.getElementById("result").innerText=Number(number1.value)-Number(number2.value)}
+    
+    
+    
+    function mile(){document.getElementById("result2").innerHTML=Number(number3.value)*0.621 }
+    function km(){document.getElementById("result2").innerHTML=Number(number4.value)*1.609 }
+
